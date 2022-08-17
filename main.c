b7ee544c1070707433fc97093e343ae69deff77a
UserDev987 <UserDev987@gmail.com>
2022-08-17 17:57:01 +0200

    commit

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+void drawTable(void);
+
+
+char table[2][2] = {
+    {'0', '0', '0'},
+    {'0', '0', '0'},
+    {'0', '0', '0'},
+};
+
 int main(int argc, char const *argv[])
 {
     /* code */
     return 0;
 }
+
+void drawTable(void) {
+    printf("");
+}
+
+
